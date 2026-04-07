02d15a64febde81a7f86d681821c3ede311b63c6
Imtiaz Ahmad <imtiazahmadadrali@gmail.com>
2026-04-06 20:49:50 -0400

    Extacted duplicate code into private methods & added builder annotation to the message and conversation classes

--- src/main/java/com/imti/ChatiIQ/entity/Conversation.java
+++ src/main/java/com/imti/ChatiIQ/entity/Conversation.java
@@ -1,11 +1,17 @@
 package com.imti.ChatiIQ.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Data
 public class Conversation {
